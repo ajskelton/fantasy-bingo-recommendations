b89b1a61239db6f5fecf4ea747f43ac6701c8490
Anthony Skelton <anthony@anthonyskelton.com>
2022-04-16 15:03:03 -0700

    Adds first two descriptions for Squares

--- database/seeders/SquareSeeder.php
+++ database/seeders/SquareSeeder.php
@@ -9,8 +9,8 @@ use Illuminate\Database\Seeder;
 class SquareSeeder extends Seeder
 {
     public array $squares = [
-        [ 'position' => 1, 'name' => 'LGBTQIA List', ],
-        [ 'position' => 2, 'name' => 'Weird Ecology', ],
+        [ 'position' => 1, 'name' => 'LGBTQIA List', 'description' => "**A Book from** [r/Fantasy](https://www.reddit.com/r/Fantasy/)'s **Top LGBTQIA List:** Any [book on this list](https://www.reddit.com/r/Fantasy/comments/ifmx04/rfantasys_2020_top_lgbtqa_books_voting_results/), including sequels. **HARD MODE**: A book or series that received ten votes or less." ],
+        [ 'position' => 2, 'name' => 'Weird Ecology', 'description' => 'Story takes place in a world that is wildly different from our own and includes such things as unique environments, strange flora and fauna, unusual ecosystems, etc. The difference in environment, flora and fauna, and ecosystems cannot simply be “it’s a fantasy world,” but something that is fundamentally different about the world itself. Example: The Bone Ships by RJ Barker counts as this is a poisonous world without trees and the world had to evolve in significantly different ways to deal with that. Meanwhile The Liveship Traders by Robin Hobb would not count, as it is fairly close to our own world’s ecology just with the added presence of dragons. **HARD MODE**: Not written by Jeff VanderMeer or China Miéville.' ],
         [ 'position' => 3, 'name' => 'Two or More Authors', ],
         [ 'position' => 4, 'name' => 'Historical SFF', ],
         [ 'position' => 5, 'name' => 'Set in Space', ],
